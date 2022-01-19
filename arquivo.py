4dd2f0df3ebd218bc6e5fd71ffeac68335ccf3d5
Natalia <nataliaartini@hotmail.com>
2022-01-19 18:34:49 -0300

    adiciona novos prints

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,7 @@
 print('Hello guys!')
 #comente seus códigos parça
 """documentação também é importante"""
+print("Natalia", "Ferrandin", sep="_")
+
+print('000', '000', '000', sep='.', end='-')
+print('01')
