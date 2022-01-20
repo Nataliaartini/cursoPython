3b2fbac6434eed24648709ca753bffbb94d30884
Natalia <nataliaartini@hotmail.com>
2022-01-20 08:07:14 -0300

    strings e int

--- aulasiniciais/arquivo.py
+++ aulasiniciais/arquivo.py
@@ -1,7 +1,14 @@
+#aula 1
 print('Hello guys!')
 #comente seus códigos parça
 """documentação também é importante"""
 print("Natalia", "Ferrandin", sep="_")
 
+#aula 2
 print('000', '000', '000', sep='.', end='-')
 print('01')
+
+#aula 3
+print("isso é 1 'string'.")
+print("isso é int")
+print(5)
