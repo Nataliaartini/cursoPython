deaf3a0ec6d3dda58baf943be8856a85bf186d8c
Natalia <nataliaartini@hotmail.com>
2022-01-20 08:26:52 -0300

    adiciona tipos de dados

--- aulasiniciais/arquivo.py
+++ aulasiniciais/arquivo.py
@@ -2,13 +2,20 @@
 print('Hello guys!')
 #comente seus códigos parça
 """documentação também é importante"""
-print("Natalia", "Ferrandin", sep="_")
+print("Natalia", "Ferrandin \n", sep="_")
 
 #aula 2
 print('000', '000', '000', sep='.', end='-')
-print('01')
+print('01 \n')
+
 
 #aula 3
 print("isso é 1 'string'.")
-print("isso é int")
 print(5)
+print("isso é int \n")
+
+#aula 4
+print(type("Natalia"))
+print(type(10))
+print(type(25.6))
+print(type(5==5))
