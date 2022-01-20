9a652cd009b248cfb2bb6da91f3fc06ab7f611e1
Natalia <nataliaartini@hotmail.com>
2022-01-20 08:46:19 -0300

    sobre operadores aritméticos

--- aulasiniciais/arquivo.py
+++ aulasiniciais/arquivo.py
@@ -2,23 +2,33 @@
 print('Hello guys!')
 #comente seus códigos parça
 """documentação também é importante"""
-print("Natalia", "Ferrandin \n", sep="_")
+print("Natalia", "Ferrandin ", sep="_")
+print(25*"-")
 
 #aula 2
 print('000', '000', '000', sep='.', end='-')
-print('01 \n')
+print('01 ')
+print(25*"-")
 
 
 #aula 3
 print("isso é 1 'string'.")
 print(5)
-print("isso é int \n")
+print("isso é int ")
+print(25*"-")
 
 #aula 4
 print('Nome: Natalia', type("Natalia"))
 print("idade:", 21, type(21))
 print("altura:",1.60, type(1.60))
-print('é maior de idade?',21>=18 , type(21>=18))
+print('é maior de idade?',21>=18 , type(21>=18),)
+print(25*"-")
 
 #aula 5
-
+print("10 x 10 =",10*10)
+print("3+8 = ", 3+8)
+print("9/3 = ", 9/3)
+print("3² = ", 3**2)
+print("divisão inteira de 15/8 = ", 15//8)
+print("resto da divisão 15/8 = ", 15%8)
+print(25*"-")
