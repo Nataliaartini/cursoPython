a967bd59fd50ee0ca50536b139dba9b80e71e8ad
Natalia <nataliaartini@hotmail.com>
2022-01-20 16:41:18 -0300

    manipulando variáveis

---
+++ aulasiniciais/format.py
@@ -0,0 +1,10 @@
+a= 1
+print(f"{a:0>10}") #preenche com 0 à esquerda
+print(f"{a:0<10}") #preenche com 0 à direita
+print(f"{a:0^10}") #deixa o numero no meio de 0
+print(f"{a:0>10.2f}")
+nome = "Natalia"
+sobrenome= "Ferrandin"
+nomeCompleto= '{1}, {0}'.format(nome, sobrenome)
+print(nomeCompleto)
+print(28 * "-")
