64e235a5a61d49f8983c4dabc953f3e531ab282d
Natalia <nataliaartini@hotmail.com>
2022-01-21 14:57:22 -0300

    adiciona contagem de palavras

--- aulasiniciais/else.py
+++ aulasiniciais/else.py
@@ -12,3 +12,19 @@ if init:
     print("existe uma palavra com inicio t")
 else:
     print("nenhuma dessas palavras começa com t")
+print(28*"-")
+
+# aula 18
+string = "eu sou uma moça, eu tenho 20 anos"
+vetor = string.split(",") #vai dividir em duas strings separadas na virgula
+vetorcount = string.split(" ")
+palavra = " "
+contagem = 0
+for n in vetorcount:
+    print(f"{n} apareceu {vetorcount.count(n)}X.")
+    qtd = vetorcount.count(n)
+    if qtd > contagem:
+        contagem = qtd
+        palavra = n
+print(28*"-")
+print(f"{palavra} {contagem}X, foi o que mais apareceu")
