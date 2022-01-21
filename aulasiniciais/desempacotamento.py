f9cb47bf2ac897afa848048941cd02303c147f7e
Natalia <nataliaartini@hotmail.com>
2022-01-21 16:48:28 -0300

    adiciona if ternario

--- aulasiniciais/desempacotamento.py
+++ aulasiniciais/desempacotamento.py
@@ -1,3 +1,19 @@
-clientes = ["maria","pedro","guilherme","alexandre"]
-c1,c2,*new_lista = clientes
-print(c1,new_lista)
+clientes = ["maria", "pedro", "guilherme", "alexandre"]
+c1, c2, *new_lista = clientes
+print(c1, new_lista)
+# *_ significa que a variavel nao sera usada
+
+a = 15
+b = "luz"
+c= "newton"
+a, b, c = c, a, b
+print(f"a= {a}, b= {b} e c= {c}")
+print(28*"-")
+
+# aula 20
+logged_user = True
+msg = "Usuário logado" if logged_user else "Usuário precisa fazer login"
+print(msg)
+print(28*"-")
+
+
