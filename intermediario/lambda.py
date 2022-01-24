a241d40f3aa82eb3671089b23595422b73daaa73
Natalia <nataliaartini@hotmail.com>
2022-01-24 17:20:28 -0300

    usando funcao lambda para ordenar

--- intermediario/lambda.py
+++ intermediario/lambda.py
@@ -11,8 +11,11 @@ lista = [
     ["p2", 5.6],
     ["p3", 9.5],
 ]
-def dizlugar(item):
-    return item[1] #indice 1 que é preco
+'''def dizlugar(item):
+    return item[1] #indice 1 que é preco'''
+
 #lista.sort(key=dizlugar) - ordena pelo menor preco
-lista.sort(key=dizlugar, reverse=True)
+lista.sort(key=lambda item: [1], reverse=True) #aqui o lambda elimina uma função só pra retornar um indice
 print(decrescente)
+'''pode ser usado a funcao print sorted lista com o lambda que é o mesmo resultado mas sem alterar lista original'''
+
