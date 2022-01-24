9776a48c803ba23f2672276cae51498281165845
Natalia <nataliaartini@hotmail.com>
2022-01-24 10:15:31 -0300

    adiciona novas funcoes

--- intermediario/def.py
+++ intermediario/def.py
@@ -9,8 +9,19 @@ def divisao(n1, n2):
     if n1 == 0 or n2 == 0:
         return
     return n1/n2
-divide = divisao(8,-1)
+
+divide = divisao(8,5)
 if divide:
     print(divide)
 else:
     print("não existe divisão por 0, tente outro numero")
+
+def dumb ():
+    return 1
+
+print(dumb(), type(dumb()))
+
+def example():
+    return ("1", "2")
+var = example()
+print(var, type(var))
