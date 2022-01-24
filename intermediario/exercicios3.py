f2e8aee52fb5442d9c10759d85552b0e906e3b24
Natalia <nataliaartini@hotmail.com>
2022-01-24 13:48:13 -0300

    executa funcoes dentro de funcoes

---
+++ intermediario/exercicios3.py
@@ -0,0 +1,21 @@
+def func():
+    return "oi pessoal"
+
+def func1(funcao):
+    return funcao()
+executa = func1(func)
+print(executa)
+
+def um(nome):
+    return f"oi {nome}"
+
+def dois(nome, saudacao):
+    return f"{saudacao} {nome}"
+
+def main(funcao, *args, **kwargs):
+    return funcao(*args, **kwargs)
+
+letsgo = main(um, "Natalia")
+letsgo1= main(dois, "Natalia", saudacao = "Bom dia")
+print(letsgo)
+print(letsgo1)
