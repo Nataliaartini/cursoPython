702ada0585cc706c732a92e237e1379a1dfd6370
Natalia <nataliaartini@hotmail.com>
2022-01-24 09:44:50 -0300

    funcao dividir com tratamento

--- intermediario/def.py
+++ intermediario/def.py
@@ -1,6 +1,16 @@
-def saudacao (msg, nome):
+'''def saudacao (msg, nome):
     nome = nome.replace("a", "é")
     return f'{msg} {nome}'
 
 var = saudacao("Boa tarde", "Natalia")
-print (var)
+print (var)'''
+
+def divisao(n1, n2):
+    if n1 == 0 or n2 == 0:
+        return
+    return n1/n2
+divide = divisao(8,-1)
+if divide:
+    print(divide)
+else:
+    print("não existe divisão por 0, tente outro numero")
