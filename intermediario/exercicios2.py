11cafe3fd7a5deb7706933ce69e3dc03bfd849b1
Natalia <nataliaartini@hotmail.com>
2022-01-24 11:17:45 -0300

    resolve exercicios de funcao

--- intermediario/exercicios2.py
+++ intermediario/exercicios2.py
@@ -17,12 +17,12 @@ print(preco)
 def fizzbuzz(g):
     if g%3 == 0 and g%5 == 0:
         return "fizzbuzz"
-    elif g%5 == 0:
+    if g%5 == 0:
         return "buzz"
-    elif g%3 == 0:
+    if g%3 == 0:
         return "fizz"
-    else:
-        return g
+
+    return g
 teste = fizzbuzz(15)
 print(teste)
 
