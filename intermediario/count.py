ebc07daff3202d181fa7102e5ee1416d33727809
Natalia <nataliaartini@hotmail.com>
2022-01-25 17:29:35 -0300

    agrupa alunos pela nota

--- intermediario/count.py
+++ intermediario/count.py
@@ -20,3 +20,19 @@ for grupo in it.combinations(alunos, 2):
 print(45*"-")
 for grupos in it.permutations(alunos,2):
     print(grupos)
+print(45*"-")
+estudante = [
+    {"nome":"ana", "nota": 7.3},
+    {"nome":"luiz", "nota": 6.9},
+    {"nome":"cleiton", "nota": 9.1},
+    {"nome":"pedro", "nota": 8.5},
+    {"nome":"clau", "nota": 8.5},
+    {"nome":"everton", "nota": 8.5},
+]
+ordena = lambda item: item["nota"]
+estudante.sort(key=ordena) #tem que estar ordenado para usar o groupby e funcionar
+agrupados = it.groupby(estudante, ordena)
+for group, valores in agrupados:
+    print(f"grupo: {group}")
+    qtd = len(list(valores))
+    print(f"{qtd} alunos tiraram a nota {group}")
