95b2fc52095dc673422a00716f0d6ef862b016b5
Natalia <nataliaartini@hotmail.com>
2022-01-25 11:44:43 -0300

    divide string com comprehension

---
+++ intermediario/exercicio5.py
@@ -0,0 +1,6 @@
+string = "012345678901234567890123456789012345678901234567890123456789"
+n = 10
+l1 = [string[i: i+n] for i in range(0, len(string), n)]
+print(l1)
+retorno = '.'.join(l1)
+print(retorno)
