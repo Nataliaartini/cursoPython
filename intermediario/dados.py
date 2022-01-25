2f10f4503bc2d4cc79e1081edc27d0f2dbce89e3
Natalia <nataliaartini@hotmail.com>
2022-01-25 17:37:35 -0300

    dados para serem tratados nos testes

---
+++ intermediario/dados.py
@@ -0,0 +1,28 @@
+produtos = [
+    {"nome": "p1", "preco": 50.5},
+    {"nome": "p2", "preco": 60.9},
+    {"nome": "p3", "preco": 59.99},
+    {"nome": "p4", "preco": 35},
+    {"nome": "p5", "preco": 64},
+    {"nome": "p6", "preco": 10},
+    {"nome": "p7", "preco": 0.9},
+    {"nome": "p8", "preco": 7},
+    {"nome": "p9", "preco": 5},
+    {"nome": "p10", "preco": 18.8},
+    {"nome": "p11", "preco": 89},
+    {"nome": "p12", "preco": 122,85},
+    {"nome": "p13", "preco": 61,2},
+]
+pessoas = [
+    {"nome": "beatriz", "idade": 15},
+    {"nome": "duda", "idade": 23},
+    {"nome": "tiago", "idade": 18},
+    {"nome": "alice", "idade": 60},
+    {"nome": "enzo", "idade": 51},
+    {"nome": "janaina", "idade": 21},
+    {"nome": "ana paula", "idade": 32},
+    {"nome": "natasha", "idade": 26},
+    {"nome": "anderson", "idade": 18},
+    {"nome": "tuane", "idade": 30},
+]
+lista = [1,2,,3,4,5,6,7,8,9,10]
