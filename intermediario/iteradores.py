2394a82cb5def64f496c1f10f80952fe1f003795
Natalia <nataliaartini@hotmail.com>
2022-01-25 14:36:10 -0300

    testa comportamentos de geradores

--- intermediario/iteradores.py
+++ intermediario/iteradores.py
@@ -9,7 +9,7 @@ def gera():
     r = []
     for n in range(10):
         yield n
-        time.sleep(0.1) #vai demorar 1 segundo para mostrar resultado na tela
+        #time.sleep(0.1) #vai demorar 1 segundo para mostrar resultado na tela
 
 g = gera ()
 for v in g:
@@ -33,3 +33,17 @@ print(type(l1))
 print(type(l2))
 print(sys.getsizeof(l1))
 print(sys.getsizeof(l2))
+print(40*"*")
+nome = "Lua"
+iterador = iter(nome)
+gerador = (letra for letra in nome)
+for letra in gerador:
+    print(letra)
+print("-"* 5)
+try: #se acaso eu der mais nexts do que as iteracoes que eu tenho, nao vai dar erro de stopiteration
+    print(next(iterador))
+    print(next(iterador))
+    print(next(iterador))
+    print(next(iterador)) #é exatamente o que o for faz
+except:
+    pass
