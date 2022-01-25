15a5b3cd0a98b899be8724ec202e4be5b55e4c3a
Natalia <nataliaartini@hotmail.com>
2022-01-25 16:40:19 -0300

    utilizando combinations e permutations

--- intermediario/count.py
+++ intermediario/count.py
@@ -1,15 +1,22 @@
 import types
-import itertools
+import itertools as it
 var = ((x, y) for x, y in zip("oi", "oi"))
 print(isinstance(var, types.GeneratorType))
-contador = itertools.count(start=3, step=3)
+contador = it.count(start=3, step=3)
 for i in contador:
     print(i)
     if i >= 15:
         break
 print(25*"-")
-exemplo = itertools.count(start=0.5, step=0.05)
+exemplo = it.count(start=0.5, step=0.05)
 for j in exemplo:
     print(round(j, 2))
     if j >= 1:
         break
+print(45*"-")
+alunos = ["ana", "luiz", "cleiton", "pedro", "bruna", "carol", "joana", "bia", "joao", "maria"]
+for grupo in it.combinations(alunos, 2):
+    print(grupo)
+print(45*"-")
+for grupos in it.permutations(alunos,2):
+    print(grupos)
