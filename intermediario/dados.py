46515c40f49aab87a4397413842e4aaf95dd1c81
Natalia <nataliaartini@hotmail.com>
2022-01-25 17:59:03 -0300

    manipulando funcao map

--- intermediario/dados.py
+++ intermediario/dados.py
@@ -25,4 +25,4 @@ pessoas = [
     {"nome": "anderson", "idade": 18},
     {"nome": "tuane", "idade": 30},
 ]
-lista = [1,2,,3,4,5,6,7,8,9,10]
+lista = [1,2,3,4,5,6,7,8,9,10]
--- intermediario/map.py
+++ intermediario/map.py
@@ -1,3 +1,8 @@
 from dados import produtos, pessoas, lista
-nova_lista = map(lambda x: x*2)
-print(nova_lista)
+#nova_lista = map(lambda x: x*2, lista)
+new_list = [x*2 for x in lista] #faz a mesma coisa que em cima com comprehension
+print(lista)
+print(list(new_list))
+precos = map(lambda p: p["preco"], produtos)
+for preco in precos:
+    print(preco)
