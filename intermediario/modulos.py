89d06619298513763c4f739134591f9409cc3f37
Natalia <nataliaartini@hotmail.com>
2022-01-26 14:44:41 -0300

    corrige erros de apontamento e callable objects

--- intermediario/modulos.py
+++ intermediario/modulos.py
@@ -4,28 +4,37 @@ import math
 import datetime
 from dados import *
 import meumodulo
-from vendas import precos
-from vendas.formata import preco
+from vendas import preco, precos
 PI = math.pi
 print(sys.platform)
-print(rd.randint(0,13))
+print(rd.randint(0, 13))
+
+
 def dobra(lista):
     return [x*2 for x in lista]
+
+
 print(dobra(lista))
+
+
 def multiplica(lista):
     r = 1
     for i in lista:
-        r*=i
+        r *= i
     return r
+
+
 print(multiplica(lista))
 print(PI)
 print(meumodulo.mensagem())
 data = datetime.datetime.now()
 dataformata = data.strftime("%d/%m/%Y %H:%M")
-print(f"estamos em :",dataformata)
+print(f"estamos em :", dataformata)
 print(45*"*")
 
-preco = 49.9
-novopreco = precos.aumento(preco,15)
+valor = 49.9
+novopreco = precos.aumento(valor, 15)
 print(novopreco)
+precofinal = preco.real(novopreco)
+print(precofinal)
 print(45*"*")
--- intermediario/vendas/precos.py
+++ intermediario/vendas/precos.py
@@ -1,6 +1,3 @@
-import formata.preco
 def aumento(valor, porcentagem):
-    r = valor + (valor * (porcentagem/100))
-    r = formata.preco.real(r)
+    r = valor + (valor * (porcentagem / 100))
     return r
-
