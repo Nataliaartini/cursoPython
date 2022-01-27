4d4f297a4ef8b466cfe45bc4ffbd1adee1c20241
Natalia <nataliaartini@hotmail.com>
2022-01-27 10:24:06 -0300

    validador de CNPJ

---
+++ intermediario/exercicios/valida_CNPJ/desafio_CNPJ.py
@@ -0,0 +1,7 @@
+import funcoes_CNPJ
+
+cnpj = funcoes_CNPJ.valida("12.321.593/0001-77")
+cnpj1 = funcoes_CNPJ.valida("04.252.011/0001-10")
+cnpj2 = funcoes_CNPJ.valida("00.000.000/0000-00")
+cnpj3 = funcoes_CNPJ.valida("123")
+cnpj4 = funcoes_CNPJ.valida("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
---
+++ intermediario/exercicios/valida_CNPJ/funcoes_CNPJ.py
@@ -0,0 +1,54 @@
+import re
+
+REGRESSIVOS = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+def valida(cnpj):
+    cnpj = rm_pontuacao(cnpj)
+    print("\n",cnpj)
+    try:
+        if not_sequencia(cnpj):
+            print("CNPJ inválido, não pode ser sequencial")
+            return cnpj
+    except:
+        print("CNPJ INVÁLIDO")
+        return False
+    try:
+        aux_cnpj = calcula(cnpj, 1)
+        aux_cnpj = calcula(aux_cnpj, 2)
+    except Exception as e:
+        print("CNPJ INVÁLIDO")
+        return False
+    if aux_cnpj == cnpj:
+        print("CNPJ validado")
+    else:
+        print("CNPJ inválido")
+
+
+def calcula(cnpj, digito):
+    if digito == 1:
+        regressivos = REGRESSIVOS[1:]
+        aux_cnpj = cnpj[:-2]
+    elif digito == 2:
+        regressivos = REGRESSIVOS
+        aux_cnpj = cnpj
+    else:
+        return None
+    total = 0
+    for indice, regressivo in enumerate(regressivos):
+        total += int(cnpj[indice]) * regressivo
+    digito = 11 - (total % 11)
+    digito = digito if digito <= 9 else 0
+    return f"{aux_cnpj}{digito}"
+
+
+def not_sequencia(cnpj):
+    sequencia = cnpj[0] * len(cnpj)
+    if sequencia == cnpj:
+        return True
+    else:
+        return False
+
+
+def rm_pontuacao(cnpj):
+    return re.sub(r"[^0-9]", "", cnpj)
