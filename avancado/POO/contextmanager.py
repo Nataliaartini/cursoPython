1540ea5bd7548caa6825ec499286f141c6f9cfc8
Natalia <nataliaartini@hotmail.com>
2022-01-31 08:49:51 -0300

    criando gerenciadores de arquivo com classe e função

---
+++ avancado/POO/contextmanager.py
@@ -0,0 +1,39 @@
+'''arquivo = open("abc.txt", "w")
+arquiv.write("alguma coisa")
+arquivo.close()'''
+
+#with open("ab.txt", "w") as arquivo:
+#   arquivo.write("alguma coisa")
+
+class Arq:
+    def __init__(self, arquivo, modo):
+        print("abre arquivo")
+        self.arquivo = open(arquivo, modo)
+
+    def __enter__(self):
+        print("retorna arquivo")
+        return self.arquivo
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("fecha arquivo")
+        self.arquivo.close()
+
+with Arq("ab.txt", "w") as arquivo:
+   arquivo.write("alguma coisa")
+
+print(45*"*")
+
+from contextlib import contextmanager
+
+@contextmanager
+def novoArquivo(arq, mode):
+    try:
+        novoArquivo = open(arq, mode)
+        print("abrindo arquivo")
+        yield novoArquivo
+    finally:
+        print("arquivo fechado")
+        novoArquivo.close()
+
+with novoArquivo("abc.txt", "w") as newArquivo:
+    newArquivo.write ("primeira linha")
