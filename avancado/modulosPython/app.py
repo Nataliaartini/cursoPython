fd51a2a77457f937deb38fc53832c83055dfc0dd
Natalia <nataliaartini@hotmail.com>
2022-01-31 14:06:38 -0300

    funcao do name == main

---
+++ avancado/modulosPython/app.py
@@ -0,0 +1,4 @@
+import funcao
+
+print(funcao.soma(50, 36))
+print(funcao.soma(25, 189))
---
+++ avancado/modulosPython/funcao.py
@@ -0,0 +1,8 @@
+def soma(x, y):
+    return x + y
+
+def main() -> None:
+    print(soma(9, 15))
+
+if __name__ == "__main__":
+    main()
