565683efb64983410f15e6bcd23c37dfc6d0377a
Natalia <nataliaartini@hotmail.com>
2022-01-31 14:55:28 -0300

    formatando datas

--- avancado/modulosPython/datas.py
+++ avancado/modulosPython/datas.py
@@ -1,5 +1,8 @@
 import datetime as dt
+import locale
+from calendar import mdays
 
+locale.setlocale(locale.LC_ALL, "")
 data1 = dt.datetime(2022, 2, 6, 14, 1)
 print(data1.strftime("%d/%m/%Y %H:%M"))
 
@@ -11,5 +14,11 @@ print(data3.strftime("%d/%m/%Y"))
 print(dt.datetime.weekday(data3)) #imprime em enum de 0 à 6 começando por segunda
 data4 = data3 - data2
 print(data4)
+print(45*"*")
 
-
+formatada = dt.datetime.now().strftime("%A, %d de %B de %Y") #dia da semana, B é mês por extenso
+formatada1 = dt.datetime.now().strftime("%d/%m/%y, %A - feira")
+print(formatada)
+print(formatada1)
+mes_atual = int(dt.datetime.now().strftime("%m"))
+print(mes_atual, "->", mdays[mes_atual]) #imprime o mês e o ultimo dia do mes atual
