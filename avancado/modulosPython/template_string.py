839e469b6e35c418900e3ab6080064e206b0796a
Natalia <nataliaartini@hotmail.com>
2022-02-01 09:21:06 -0300

    envio de email

--- avancado/modulosPython/template_string.py
+++ avancado/modulosPython/template_string.py
@@ -3,11 +3,29 @@ from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+import smtplib
+
+
+meu_email = "digite o email que será usado para enviar o email"
+senha = "digite a senha desse email"
+email_recebe = "email do cliente"
 
 with open ("template_email.html", "r") as html:
     template = Template(html.read())
     data_atual = datetime.now().strftime("%d/%m/%Y")
     corpo_msg = template.safe_substitute(nome = "Mago Supremo", data = data_atual)
-print(corpo_msg)
 
+msg = MIMEMultipart()
+msg["from"] = "Natalia Artini Ferrandin"
+msg["to"] = email_recebe
+msg["subject"] = "EMAIL DE TESTES"
 
+corpo = MIMEText(corpo_msg, "html")
+msg.attach(corpo)
+email = msg
+with smtplib.SMTP(host= "smtp.gmail.com", port = 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(meu_email, senha)
+    smtp.send_message(msg)
+    print("email enviado com sucesso")
