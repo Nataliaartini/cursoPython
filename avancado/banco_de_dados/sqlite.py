4ed8096098a944f8a764c92e9daf7ae73f1c57f7
Natalia <nataliaartini@hotmail.com>
2022-02-02 08:06:45 -0300

    update and delete

--- avancado/banco_de_dados/sqlite.py
+++ avancado/banco_de_dados/sqlite.py
@@ -7,7 +7,9 @@ cursor = conexao.cursor()
 #                "id INTEGER PRIMARY KEY AUTOINCREMENT, "
 #                "nome TEXT, "
 #                "peso REAL"
-#                ")") uma vez criada a tabela posso comentar pra nao ficar executando toda vez
+#                ")")
+#
+#                uma vez criada a tabela posso comentar pra nao ficar executando toda vez
 
 # cursor.execute("INSERT INTO clientes (nome, peso) VALUES (?, ?)", ("Nathaly", 55.2))
 # cursor.execute("INSERT INTO clientes (nome, peso) VALUES (:nome, :peso)", {"nome": "Eduarda", "peso": 65.9})
@@ -17,13 +19,23 @@ cursor = conexao.cursor()
 #
 # conexao.commit()
 
+cursor.execute("UPDATE clientes SET nome=:nome WHERE id=:id", {"nome": "Ricardo", "id": "2"})
+conexao.commit()
+
+# cursor.execute("DELETE FROM clientes WHERE id=:id", {"id": "2"})
+# conexao.commit()
+
 cursor.execute("SELECT * FROM clientes")
 
 for linha in cursor.fetchall():
     identificador, nome, peso = linha
     print(identificador, nome, peso)
 
+cursor.execute("SELECT nome, peso FROM clientes WHERE peso > 80")
 
+for linha in cursor.fetchall():
+    nome, peso = linha
+    print(nome, peso)
 
 
 
