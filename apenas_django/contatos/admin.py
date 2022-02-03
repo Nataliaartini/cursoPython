c04fd024a19ba48905aac809cc2b160f96f3cca5
Natalia <nataliaartini@hotmail.com>
2022-02-03 15:57:40 -0300

    altera colors e identação

--- apenas_django/contatos/admin.py
+++ apenas_django/contatos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Categoria, Contato
 
+
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ("id", "nome", "sobrenome", "telefone", "categoria")
     list_display_links = ("id", "nome", "sobrenome")
