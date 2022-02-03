56aed4a0d561235bf97898840517be9a231d271e
Natalia <nataliaartini@hotmail.com>
2022-02-03 09:47:40 -0300

    adicionando css js

--- apenas_django/newsite/example/views.py
+++ apenas_django/newsite/example/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'example/index.html')
+
+def sobre(request):
+    return render(request, 'example/sobre.html')
--- apenas_django/newsite/newsite/views.py
+++ apenas_django/newsite/newsite/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+def sobre(request):
+    return render(request, 'paginas/about.html')
