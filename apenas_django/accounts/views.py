3ad04297e2151f18341fe94ceea3cca1cd567d2c
Natalia <nataliaartini@hotmail.com>
2022-02-04 17:55:39 -0300

    valida email

--- apenas_django/accounts/views.py
+++ apenas_django/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.validators import validate_email
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -8,6 +9,27 @@ def logout(request):
     return render(request, 'accounts/logout.html')
 
 def cadastro(request):
+    if request.method != 'POST':
+        messages.info(request, 'NADA POSTADO.')
+        return render(request, 'accounts/cadastro.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuário')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
+        messages.error(request, "Todos os campos são obrigatórios.")
+        return render(request, "accounts/cadastro.html")
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, "Email inválido.")
+        return render(request, "accounts/cadastro.html")
+
     return render(request, 'accounts/cadastro.html')
 
 def dashboard(request):
