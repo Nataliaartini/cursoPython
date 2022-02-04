dfb79d20740e26dd721fd995acaa60bccae0b8f4
Natalia <nataliaartini@hotmail.com>
2022-02-04 09:26:52 -0300

    ordena por nome

--- apenas_django/contatos/views.py
+++ apenas_django/contatos/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('nome').filter(mostrar=True)
     paginator = Paginator(contatos, 4)
     page = request.GET.get("p")
     contatos = paginator.get_page(page)
@@ -16,6 +16,8 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:
+        raise Http404()
     return render(request, 'contatos/ver_contato.html', {
         "contato": contato
     })
