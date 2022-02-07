9932341f25167f7bb55a3bc0ec0b6cb9bf0366b7
Natalia <nataliaartini@hotmail.com>
2022-02-07 08:41:52 -0300

    adiciona validações para cadastro

--- apenas_django/accounts/views.py
+++ apenas_django/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.core.validators import validate_email
+from django.contrib.auth.models import User
 
 def login(request):
     return render(request, 'accounts/login.html')
@@ -10,27 +11,47 @@ def logout(request):
 
 def cadastro(request):
     if request.method != 'POST':
-        messages.info(request, 'NADA POSTADO.')
         return render(request, 'accounts/cadastro.html')
 
     nome = request.POST.get('nome')
     sobrenome = request.POST.get('sobrenome')
     email = request.POST.get('email')
-    usuario = request.POST.get('usuário')
+    usuario = request.POST.get('usuario')
     senha = request.POST.get('senha')
     senha2 = request.POST.get('senha2')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, "E-mail inválido.")
+        return render(request, "accounts/cadastro.html")
 
     if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
         messages.error(request, "Todos os campos são obrigatórios.")
         return render(request, "accounts/cadastro.html")
 
-    try:
-        validate_email(email)
-    except:
-        messages.error(request, "Email inválido.")
+    if len(senha) < 6:
+        messages.error(request, "Senha deve ter no mínimo 6 caracteres.")
         return render(request, "accounts/cadastro.html")
 
-    return render(request, 'accounts/cadastro.html')
+    if senha != senha2:
+        messages.error(request, "Senhas devem ser iguais")
+        return render(request, "accounts/cadastro.html")
+
+    if len(usuario) < 6:
+        messages.error(request, "Usuário deve ter no mínimo 6 caracteres.")
+        return render(request, "accounts/cadastro.html")
+
+    if User.objects.filter(username=usuario).exists():
+        messages.error(request, "Este nome de usuário já está em uso.")
+        return render(request, "accounts/cadastro.html")
+
+    if User.objects.filter(email=email).exists():
+        messages.error(request, "Este e-mail já está cadastrado.")
+        return render(request, "accounts/cadastro.html")
+
+    messages.success(request, "Registrado com sucesso!")
+    return render(request, "accounts/cadastro.html")
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
