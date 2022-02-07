520c9c650a6ef8ccd69b80c106840f41219085ee
Natalia <nataliaartini@hotmail.com>
2022-02-07 10:06:12 -0300

    adiciona nome de página

--- apenas_django/accounts/views.py
+++ apenas_django/accounts/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+    usuario = request.POST.get("usuario")
+    senha = request.POST.get("senha")
+
+    user = auth.authenticate(request, username=usuario, password=senha)
+
+    if not user:
+        messages.error(request, "Usuário ou senha inválidos")
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        messages.success(request, "Login efetuado com sucesso!")
+        return redirect('dashboard')
 
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    return redirect('dashboard')
 
 def cadastro(request):
     if request.method != 'POST':
@@ -52,9 +68,10 @@ def cadastro(request):
 
     messages.success(request, "Registrado com sucesso! Agora você já pode entrar na sua conta.")
 
-    user= User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=sobrenome)
+    user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=sobrenome)
     user.save()
     return redirect('login')
 
+@login_required(redirect_field_name='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
