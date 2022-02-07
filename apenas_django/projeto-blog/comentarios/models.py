c8f8c1658a1ecb4cc116e1e395343681512ae3fc
Natalia <nataliaartini@hotmail.com>
2022-02-07 17:54:13 -0300

    comentarios

--- apenas_django/projeto-blog/comentarios/models.py
+++ apenas_django/projeto-blog/comentarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comentario(models.Model):
+    nome_comentario = models.CharField(max_length=150)
+    email_comentario = models.EmailField()
+    comentario = models.TextField()
+    post_comentario = models.ForeignKey()
+    usuario_comentario = models
+    data_comentario = models
+    publicado_comentario = models
