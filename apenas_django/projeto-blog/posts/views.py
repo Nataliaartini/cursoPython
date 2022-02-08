14fc8cef10b37022a34fb7f7ce6cf00aff7c4618
Natalia <nataliaartini@hotmail.com>
2022-02-08 16:21:12 -0300

    posta comentarios em posts

--- apenas_django/projeto-blog/posts/views.py
+++ apenas_django/projeto-blog/posts/views.py
@@ -7,6 +7,7 @@ from comentarios.forms import FormComentario
 from comentarios.models import Comentario
 from django.contrib import messages
 
+
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
@@ -26,6 +27,7 @@ class PostIndex(ListView):
 
         return qs
 
+
 class PostBusca(PostIndex):
     template_name = 'posts/post_busca.html'
 
@@ -43,23 +45,34 @@ class PostBusca(PostIndex):
 
         return qs
 
+
 class PostCategoria(PostIndex):
     template_name = 'posts/post_categoria.html'
-    
+
     def get_queryset(self):
         qs = super().get_queryset()
-        categoria = self.kwargs.get('categoria', None))
+        categoria = self.kwargs.get('categoria', None)
+
         if not categoria:
             return qs
 
         qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
         return qs
 
+
 class PostDetalhes(UpdateView):
     template_name = 'posts/post_detalhes.html'
     model = Post
     form_class = FormComentario
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comentarios = Comentario.objects.filter(publicado_comentario=True, post_comentario=post.id)
+        contexto['comentarios'] = comentarios
+
+        return contexto
 
     def form_valid(self, form):
         post = self.get_object()
