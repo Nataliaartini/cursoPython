2dcb76876cf6f7a7f3c23e8f9bbbed107b1a3f25
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2022-02-09 15:24:45 -0300

    remove secret key

--- e-commerce_django/loja/settings.py
+++ e-commerce_django/loja/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '34s66f2twc8c&q*i_@@4i(yp7wp0-d_hi3il7%1iom55xz(87w'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
