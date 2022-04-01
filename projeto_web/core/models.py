f7ef2f073cf7a5831e29ac9b7b85275c42c7506f
Natalia <nataliaartini@hotmail.com>
2022-04-01 10:00:28 -0300

    novo model criado

--- projeto_web/core/models.py
+++ projeto_web/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField("Nome", max_length=100)
+    preco = models.FloatField("Preco")
+    estoque = models.IntegerField("estoque inteiro apenas")
+
+
+class Cliente(models.Model):
+    nome = models.CharField("Nome", max_length=50)
+    sobrenome = models.CharField("Sobrenome", max_length=100)
+    email = models.EmailField("E-mail", max_length=100)
