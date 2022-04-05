c099aa7f43a4e2e9149db734fa462c121f342690
Natalia <nataliaartini@hotmail.com>
2022-04-05 17:45:41 -0300

    adicionando tratamento para paginas de erros

--- projeto_web/core/views.py
+++ projeto_web/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Produto
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     produtos = Produto.objects.all
@@ -21,3 +23,11 @@ def produto(request, pk): #pk que é passado no url de acesso a pagina produto u
         'produto': product
     }
     return render(request, 'produto.html', context) #o context é o que vai mostrar o meu produto na pagina produto quando eu clico na sua url
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text.html; charset=utf8', status=500)
