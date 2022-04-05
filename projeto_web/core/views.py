dbb025c74304bf7f501f94671fc48741449fb230
Natalia <nataliaartini@hotmail.com>
2022-04-05 16:55:17 -0300

    atualizando

--- projeto_web/core/views.py
+++ projeto_web/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Produto
+from django.shortcuts import get_object_or_404
 
 def index(request):
     produtos = Produto.objects.all
@@ -14,7 +15,8 @@ def contato(request):
     return render(request, "contato.html")
 
 def produto(request, pk): #pk que é passado no url de acesso a pagina produto urlpattern
-    product = Produto.objects.get(id=pk) #id é o que está no index, quero que o id seja igual o pk que passei para Produto em urlpattern
+    #product = Produto.objects.get(id=pk) #id é o que está no index, quero que o id seja igual o pk que passei para Produto em urlpattern
+    product = get_object_or_404(Produto, id=pk) #agora quando a pagina nao existir usuario recebe um 404
     context = {
         'produto': product
     }
